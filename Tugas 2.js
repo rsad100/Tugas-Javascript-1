c9e8c7ecb18bb5f52e0c025bb7a848e800628906
rsad100 <113919842+rsad100@users.noreply.github.com>
2022-09-23 07:32:08 +0700

    Add files via upload

--- Tugas 2.js
+++ Tugas 2.js
@@ -6,25 +6,25 @@ const ipa = 69;
 let ratarata = (mtk + bahasaIndonesia + bahasaInggris + ipa)/4;
 let grade;
 
-if ( isNaN(mtk, bahasaIndonesia, bahasaInggris, ipa) == false){
+if ( typeof mtk == "number" && typeof bahasaIndonesia == "number" && typeof bahasaInggris == "number" && typeof ipa == "number"){
 
     if ( 90 <= ratarata && ratarata <= 100 ){
         grade = "A";
         console.log(grade);
         console.log(ratarata);
-    } else if ( 80 <= ratarata && ratarata <= 89 ){
+    } else if ( 80 <= ratarata && ratarata <= 90 ){
         grade ="B";
         console.log(grade);
         console.log(ratarata);
-    } else if ( 70 <= ratarata && ratarata <= 79){
+    } else if ( 70 <= ratarata && ratarata < 80){
         grade = "C";
         console.log(grade);
         console.log(ratarata);
-    } else if ( 60 <= ratarata && ratarata <= 69){
+    } else if ( 60 <= ratarata && ratarata <= 70){
         grade = "D";
         console.log(grade);
         console.log(ratarata);
-    } else if ( 0 <= ratarata && ratarata <= 59){
+    } else if ( 0 <= ratarata && ratarata <= 60){
         grade = "E";
         console.log(grade);
         console.log(ratarata);
