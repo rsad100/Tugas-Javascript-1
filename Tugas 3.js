cedd9b797bcbc26e4295423d81d7b23525f4e664
rsad100 <113919842+rsad100@users.noreply.github.com>
2022-09-23 07:45:43 +0700

    Add files via upload

--- Tugas 3.js
+++ Tugas 3.js
@@ -1,13 +1,18 @@
 let star = "";
 
-function printSegitiga(n) {
-for(var i=n; i >= 1; i--){
+const printSegitiga = 5;
+
+if ( typeof printSegitiga == "number"){
+for(var i=printSegitiga; i >= 1; i--){
+    
     for(var j=1, y=1; j<=i; j++, y++){
      star += y
     }
     star += "\n";
+  
   }
   console.log(star)
+} else {
+  console.log("Data tidak valid")
 }
 
-printSegitiga(7);
